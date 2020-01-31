dec793eae11645a0b0f9eedd4235ea1b6d97ea79
Sandeepsirimanae <60432248+Sandeepsirimanae@users.noreply.github.com>
2020-01-31 11:35:04 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace switch_statements
+{
+    class MyApplication
+    {
+        static void Main(string[] args)
+        {
+            int day =0;
+            switch(day)
+            {
+                case 0:
+                    Console.WriteLine("non of the above");
+                break;
+
+            case 1:
+                  Console.WriteLine("monday");
+            break;
+
+                  case 2:
+                       Console.WriteLine("tuesday");
+                  break;
+
+             case 3:
+                  Console.WriteLine("wednesday");
+             break;
+
+                  case 4:
+                       Console.WriteLine("thursday");
+                  break;
+
+            case 5:
+                 Console.WriteLine("friday");
+            break;
+
+                  case 6:
+                        Console.WriteLine("saturday");
+                  break;
+
+             case 7:
+                    Console.WriteLine("sunday");
+             break;
+
+            }
+        }
+    }
+}
